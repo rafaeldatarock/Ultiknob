c997839f5dfaab75ed7fe2d42c039bd905e6b21f
rafaeldatarock <rafaeldatarock@gmail.com>
2022-06-07 01:05:17 +0200

    Added very nice parameter smoothing by Beats Baseln

--- Source/Delay.h
+++ Source/Delay.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <vector>
+#include <JuceHeader.h>
 
 namespace dsp
 {
@@ -9,6 +10,86 @@ namespace dsp
 	{
 		return sampleRate * lengthInMs * static_cast<T>(.001);
 	}
+
+
+	// Many thanks to 'Beats basteln :3' on youtube!
+	struct Smooth
+	{
+		static constexpr float tau = 6.28318530718f;
+		static constexpr float e = 2.71828182846f;
+
+		static void makeFromDecayInSamples(Smooth& s, float d) noexcept
+		{
+			const auto x = std::pow(e, -1.f / d);
+			s.setX(x);
+		}
+		static void makeFromDecayInSecs(Smooth& s, float d, float Fs) noexcept
+		{
+			makeFromDecayInSamples(s, d * Fs);
+		}
+		static void makeFromDecayInFc(Smooth& s, float fc) noexcept
+		{
+			const auto x = std::pow(e, -tau * fc);
+			s.setX(x);
+		}
+		static void makeFromDecayInHz(Smooth& s, float d, float Fs) noexcept
+		{
+			makeFromDecayInFc(s, d / Fs);
+		}
+		static void makeFromDecayInMs(Smooth& s, float d, float Fs) noexcept
+		{
+			makeFromDecayInSamples(s, d * Fs * .001f);
+		}
+
+		Smooth(const bool _snap = true, const float startVal = 0.f) :
+			a0(1.f),
+			b1(0.f),
+			y1(startVal),
+			eps(0.f),
+			snap(_snap)
+		{}
+		void reset()
+		{
+			a0 = 1.f;
+			b1 = 0.f;
+			y1 = 0.f;
+			eps = 0.f;
+		}
+		void setX(float x) noexcept
+		{
+			a0 = 1.f - x;
+			b1 = x;
+			eps = a0 * 1.5f;
+		}
+		void operator()(float* buffer, float val, int numSamples) noexcept
+		{
+			if (buffer[0] == val)
+				return juce::FloatVectorOperations::fill(buffer, val, numSamples);
+			for (auto s = 0; s < numSamples; ++s)
+				buffer[s] = processSample(val);
+		}
+		void operator()(float* buffer, int numSamples) noexcept
+		{
+			for (auto s = 0; s < numSamples; ++s)
+				buffer[s] = processSample(buffer[s]);
+		}
+		float operator()(float sample) noexcept
+		{
+			return processSample(sample);
+		}
+	protected:
+		float a0, b1, y1, eps;
+		const bool snap;
+
+		float processSample(float x0) noexcept
+		{
+			if (snap && std::abs(y1 - x0) < eps)
+				y1 = x0;
+			else
+				y1 = x0 * a0 + y1 * b1;
+			return y1;
+		}
+	};
 
 	struct WriteHead
 	{
@@ -50,7 +131,10 @@ namespace dsp
 	struct Delay
 	{
 		Delay() :
+			sampleRate(0.),
 			ringBuffer(),
+			parameterBufferLength(),
+			delayTimeSmooth(0.f),
 			writeHead(),
 			delayLength(0.f),
 			ringBufferSize(1001)
@@ -67,6 +151,8 @@ namespace dsp
 			ringBufferSize = lengthInSamples;
 
 			writeHead.prepare(blockSize, ringBufferSize);
+			parameterBufferLength.resize(blockSize);
+			Smooth::makeFromDecayInMs(delayTimeSmooth, 500.f, sampleRate);
 		}
 
 		void updateParameters(float _delayLength)
@@ -77,6 +163,8 @@ namespace dsp
 		void processBlock(float** samples, int numChannels, int numSamples)
 		{
 			writeHead(numSamples);
+
+			delayTimeSmooth(parameterBufferLength.data(), delayLength, numSamples);
 
 			for (auto channel = 0; channel < numChannels; ++channel)
 			{
@@ -99,7 +187,7 @@ namespace dsp
 					* then the readPos should be increased by the length of the buffer
 					* so that the readPos is placed near the end of the buffer
 					*/ 
-					auto readPos = static_cast<float>(writePos) - delayLength;
+					auto readPos = static_cast<float>(writePos) - parameterBufferLength[sample];
 					if (readPos < 0.f) readPos += ringBufferSize;
 					samplesSingleChannel[sample] += ringBufferSingleChannel[static_cast<int>(readPos)];
 				}
@@ -109,6 +197,8 @@ namespace dsp
 	protected:
 		double sampleRate;
 		std::array<std::vector<float>, 2> ringBuffer;
+		std::vector<float> parameterBufferLength;
+		Smooth delayTimeSmooth;
 		WriteHead writeHead;
 		float delayLength;
 		int ringBufferSize;

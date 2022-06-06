9bd0db57c71022a10759a517ca7b0cf004e20a50
rafaeldatarock <rafaeldatarock@gmail.com>
2022-06-07 01:56:37 +0200

    Moved interpolation and smoothing to seperate file and namespace

--- Source/Delay.h
+++ Source/Delay.h
@@ -1,19 +1,7 @@
 #pragma once
 #include <array>
 #include <vector>
-#include <JuceHeader.h>
-
-namespace interpolation
-{
-	inline float linearInterpolation(float* bufferChannel, float readPos, int bufferSize) noexcept
-	{
-		const auto readPosFloor = std::floor(readPos);
-		const auto floor = static_cast<int>(readPosFloor);
-		const auto ceiling = (floor + 1) % bufferSize;
-		const auto fraction = readPos - readPosFloor;
-		return bufferChannel[floor] + fraction * (bufferChannel[ceiling] - bufferChannel[floor]);
-	}
-}
+#include "Utils.h"
 
 namespace dsp
 {
@@ -22,86 +10,6 @@ namespace dsp
 	{
 		return sampleRate * lengthInMs * static_cast<T>(.001);
 	}
-
-
-	// Many thanks to 'Beats basteln :3' on youtube!
-	struct Smooth
-	{
-		static constexpr float tau = 6.28318530718f;
-		static constexpr float e = 2.71828182846f;
-
-		static void makeFromDecayInSamples(Smooth& s, float d) noexcept
-		{
-			const auto x = std::pow(e, -1.f / d);
-			s.setX(x);
-		}
-		static void makeFromDecayInSecs(Smooth& s, float d, float Fs) noexcept
-		{
-			makeFromDecayInSamples(s, d * Fs);
-		}
-		static void makeFromDecayInFc(Smooth& s, float fc) noexcept
-		{
-			const auto x = std::pow(e, -tau * fc);
-			s.setX(x);
-		}
-		static void makeFromDecayInHz(Smooth& s, float d, float Fs) noexcept
-		{
-			makeFromDecayInFc(s, d / Fs);
-		}
-		static void makeFromDecayInMs(Smooth& s, float d, float Fs) noexcept
-		{
-			makeFromDecayInSamples(s, d * Fs * .001f);
-		}
-
-		Smooth(const bool _snap = true, const float startVal = 0.f) :
-			a0(1.f),
-			b1(0.f),
-			y1(startVal),
-			eps(0.f),
-			snap(_snap)
-		{}
-		void reset()
-		{
-			a0 = 1.f;
-			b1 = 0.f;
-			y1 = 0.f;
-			eps = 0.f;
-		}
-		void setX(float x) noexcept
-		{
-			a0 = 1.f - x;
-			b1 = x;
-			eps = a0 * 1.5f;
-		}
-		void operator()(float* buffer, float val, int numSamples) noexcept
-		{
-			if (buffer[0] == val)
-				return juce::FloatVectorOperations::fill(buffer, val, numSamples);
-			for (auto s = 0; s < numSamples; ++s)
-				buffer[s] = processSample(val);
-		}
-		void operator()(float* buffer, int numSamples) noexcept
-		{
-			for (auto s = 0; s < numSamples; ++s)
-				buffer[s] = processSample(buffer[s]);
-		}
-		float operator()(float sample) noexcept
-		{
-			return processSample(sample);
-		}
-	protected:
-		float a0, b1, y1, eps;
-		const bool snap;
-
-		float processSample(float x0) noexcept
-		{
-			if (snap && std::abs(y1 - x0) < eps)
-				y1 = x0;
-			else
-				y1 = x0 * a0 + y1 * b1;
-			return y1;
-		}
-	};
 
 	struct WriteHead
 	{
@@ -164,7 +72,7 @@ namespace dsp
 
 			writeHead.prepare(blockSize, ringBufferSize);
 			parameterBufferLength.resize(blockSize);
-			Smooth::makeFromDecayInMs(delayTimeSmooth, 2000.f, sampleRate);
+			utils::Smooth::makeFromDecayInMs(delayTimeSmooth, 2000.f, sampleRate);
 		}
 
 		void updateParameters(float _delayLength)
@@ -201,7 +109,7 @@ namespace dsp
 					*/ 
 					auto readPos = static_cast<float>(writePos) - parameterBufferLength[sample];
 					if (readPos < 0.f) readPos += ringBufferSize;
-					samplesSingleChannel[sample] = interpolation::linearInterpolation(ringBufferSingleChannel, readPos, ringBufferSize);
+					samplesSingleChannel[sample] = utils::linearInterpolation(ringBufferSingleChannel, readPos, ringBufferSize);
 				}
 			}
 		}
@@ -210,7 +118,7 @@ namespace dsp
 		double sampleRate;
 		std::array<std::vector<float>, 2> ringBuffer;
 		std::vector<float> parameterBufferLength;
-		Smooth delayTimeSmooth;
+		utils::Smooth delayTimeSmooth;
 		WriteHead writeHead;
 		float delayLength;
 		int ringBufferSize;
